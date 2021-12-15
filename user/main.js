5bf282e478dd1ccc6f06c96bfa9a62bb7cc8d5cd
Jacob <89097881+jacobd082@users.noreply.github.com>
2021-12-14 20:28:28 -0600

    Add files via upload

--- user/main.js
+++ user/main.js
@@ -1,7 +1,7 @@
-https://arealbanana.comhttps://arealbanana.comhttps://arealbanana.comhttps://arealbanana.comfunction go() {
+function go() {
     localStorage.setItem('Name', document.getElementById('name').value)
     localStorage.setItem('user', 'true')
     localStorage.setItem('xp', '0')
     
-    window.open('https://bananagames.dashd.app', '_self')
+    window.open('https://www.arealbanana.com/user', '_self')
 }
